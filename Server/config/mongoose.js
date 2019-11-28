04e57ba25280ec68db97c6a5e92ce3b2ffb8261f
ItsJoel88 <imanuelnjodi@gmail.com>
2019-11-28 14:40:14 +0700

    fix mongo atlas uri

--- Server/config/mongoose.js
+++ Server/config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 module.exports = () => {
-    mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, function (err) {
+    mongoose.connect(`mongodb+srv://new-user_31:${process.env.PASSWORD_MONGO}@cluster0-96gdk.gcp.mongodb.net/react-countries?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true }, function (err) {
         if (err) console.log(err)
         else console.log('Connected to database')
     })
